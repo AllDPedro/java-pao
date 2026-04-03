299c20aa16241af9489e89d2ca473753ce248b90
AllDPedro <allan.delon199@gmail.com>
2026-04-03 13:48:41 -0300

    Upando Aula8 sobre argumentos.

--- src/Aula8.java
+++ src/Aula8.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Aula8 {
+
+    public static void main (String [] args){
+        System.out.println("Opa," + args[0]); // o (args []) é uma coleção de  indices que podem ser  passados
+
+    }
+
 }
