ee74f6d6bde70be1ea1fd15646bdf7d9748fd2d8
AllDPedro <allan.delon199@gmail.com>
2026-04-06 16:02:50 -0300

    Upando Aula10 criação de gerenciador de numeros da mega sena.

---
+++ src/Aula10.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+
+        // Gerador de números da mega sena.
+public class Aula10 {
+
+    public static void main(String[] args){
+        // criar um numero aleatorio no windows
+        Random generate = new Random();
+
+        // o while é para usar um numero especifico ou infinito
+//        int i = 0;
+//        // while (true) -> infinito usando i para por  um limite menor < ou maior > que o numero desejado
+//        while(i < 6) {
+//            int number = generate.nextInt(60);
+//            System.out.println(number);
+//            // i++ para fazer até o numero selecionado no  while
+//            i++;
+//          }
+
+        // o for é para iniciar e declarar as condições desejadas.
+        for(int i = 0; i < 6; i++) {
+            int number = generate.nextInt(60);
+            System.out.println(number);
+        }
+    }
+}
